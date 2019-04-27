aded9207b1376a925c6485221fecd10120261777
EL-HADI Ayoub <ayblhd@gmail.com>
2019-04-27 19:44:07 +0100

    UI update

--- projects/Random-Password-Generator/index.js
+++ projects/Random-Password-Generator/index.js
@@ -1,196 +1,75 @@
-//Controls
-var result = document.getElementById("result-label");
-var val = document.getElementById("val");
-var strength = document.getElementById("strength-label");
-var toast = document.getElementById("toast");
+var selectedIndex = 0;
 
-//Default values
-
-//initializing Parameters
-var includeNumbers = true;
-var includeLowerCase = true;
-var includeUpperCase = true;
-var includeSymbols = true;
-
-var passwordLength = 10;
-
-//Character strings
-var initialCharStrings = [
-    "0123456789",
-    "abcdefghijklmnopqrstuvwxyz",
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+//Database elements
+const links = [["RANDOM PASSWORD GENERATOR", "images/rpwg.png", "Random Password Generator is a simple Mobile-First web page to create secure passwords in a lazy way :) ", "https://github.com/ElhadiAyoub/Random-Password-Generator", "projects/Random-Password-Generator/index.html"],
+["COUNTDOWN TIMER", "images/cdt.png", "A simple Mobile-First Countdown timer. ", "https://github.com/ElhadiAyoub/CountDown-Timer", "projects/CountDown-Timer/index.html"],
+["TODO LIST", "images/tdl.png", "A very basic Mobile-First ToDo List. ", "https://github.com/ElhadiAyoub/TODO-List", "projects/TODO-List/index.html"],
+["COOL LOGIN FORM ANIMATED CHARACTER", "images/calfc.png", "A cool signup login form with an animated character that interacts with inputs of the user. ", "https://github.com/ElhadiAyoub/Cool-Login-Form-Animated-Character", "projects/Cool-Login-Form-Animated-Character/index.html"],
+["BOOKMARKS MANAGER", "images/bm.png", "A cool bookmark manager that lets you easily add bookmarks and manage them.", "https://github.com/ElhadiAyoub/Bookmarks-Manager", "projects/Bookmarks-Manager/index.html"],
+["EXPLORER", "images/explorer.png", "It's a static GitHub Page used as Portfolio to Showcase projects. ", "https://github.com/ElhadiAyoub/elhadiayoub.github.io", "#"]
 ];
 
-var charStrings = initialCharStrings.slice();
+//Load grid elements in DOM
+function gridLoad() {
+    const grid = document.getElementById("grid");
+    for (let i = 0; i < links.length; i++) {
+        var group = document.createElement("DIV");
+        group.classList.add("group");
+        group.style.animationDelay = 1 + i / 3 + "s";
 
-//UI selection
-function select(value, obj) {
-    if (!value) {
-        obj.style.backgroundColor = "white";
-        obj.style.color = "black";
-    }
-    else {
-        obj.style.backgroundColor = "black";
-        obj.style.color = "white";
+        var name = document.createElement("P");
+        name.innerHTML = links[i][0];
+
+        var element = document.createElement("DIV");
+        element.classList.add("item");
+        element.style.backgroundImage = "url(" + links[i][1] + ")";
+
+        var selectionId = document.createAttribute("selectionId");
+        selectionId.value = i;
+
+        group.appendChild(element);
+        group.appendChild(name);
+        group.setAttributeNode(selectionId);
+        group.addEventListener("click", showContent);
+        grid.appendChild(group);
     }
 }
+gridLoad();
 
-//Copy to clipboard
-function copy() {
-    result.select();
-    document.execCommand("copy");
-    clearSelection();
-    toast.style.display = "block";
-    toast.style.webkitAnimation = 'none';
-    setTimeout(function () {
-        toast.style.webkitAnimation = '';
-    }, 10);
-    setTimeout(function () {
-        toast.style.display = "none";
-    }, 3000);
+
+//Show player content function
+function showContent() {
+    selectedIndex = this.getAttribute("selectionId");
+    playerLoad(selectedIndex);
 }
 
-//Event listners
-var numbersButton = document.getElementById("numbers-button");
-numbersButton.addEventListener("click", function () {
-    select(includeNumbers, this);
-    includeNumbers = !includeNumbers;
-    let value = initialCharStrings[0];
-    if (includeNumbers)
-        charStrings.push(value);
-    else
-        charStrings.splice(charStrings.indexOf(value), 1);
-    getPassword();
-    result.value = word;
-    copy();
-    check();
-});
-var lowerCaseButton = document.getElementById("lowercase-button");
-lowerCaseButton.addEventListener("click", function () {
-    select(includeLowerCase, this);
-    includeLowerCase = !includeLowerCase;
-    let value = initialCharStrings[1];
-    if (includeLowerCase)
-        charStrings.push(value);
-    else
-        charStrings.splice(charStrings.indexOf(value), 1);
-    getPassword();
-    result.value = word;
-    copy();
-    check();
-});
-var upperCaseButton = document.getElementById("uppercase-button");
-upperCaseButton.addEventListener("click", function () {
-    select(includeUpperCase, this);
-    includeUpperCase = !includeUpperCase;
-    let value = initialCharStrings[2];
-    if (includeUpperCase)
-        charStrings.push(value);
-    else
-        charStrings.splice(charStrings.indexOf(value), 1);
-    getPassword();
-    result.value = word;
-    copy();
-    check();
-});
-var symbolsButton = document.getElementById("symbols-button");
-symbolsButton.addEventListener("click", function () {
-    select(includeSymbols, this);
-    includeSymbols = !includeSymbols;
-    let value = initialCharStrings[3];
-    if (includeSymbols)
-        charStrings.push(value);
-    else
-        charStrings.splice(charStrings.indexOf(value), 1);
-    getPassword();
-    result.value = word;
-    copy();
-    check();
+//Fill player elements
+function playerLoad(index) {
+    const title = document.getElementById("player-title");
+    const image = document.getElementById("player-image");
+    const text = document.getElementById("player-text");
+    const link1 = document.getElementById("player-link1");
+    const link2 = document.getElementById("player-link2");
+
+    title.innerHTML = links[index][0];
+    text.innerHTML = links[index][2];
+    image.src = links[index][1];
+    link1.href = links[index][3];
+    link2.href = links[index][4];
+
+    player.style.display = "block";
+    window.scrollTo(0, 0);
+}
+
+
+const close = document.getElementById("player-close");
+const player = document.getElementById("player");
+
+close.addEventListener("click", function () {
+    player.style.display = "none";
 });
 
-//Fisher-Yates shuffle
-function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-}
 
-shuffle(charStrings);
+const splashScreen = document.getElementById("splash-screen");
 
-//Password generator
-function objectExtract(array) {
-    return (array[Math.floor(Math.random() * array.length)]);
-}
-
-var word;
-function getPassword() {
-    if (parseInt(val.innerHTML) > 0)
-        passwordLength = parseInt(val.innerHTML);
-    word = "";
-    for (let i = 0; i < passwordLength; i++) {
-        word += objectExtract(objectExtract(charStrings));
-    }
-}
-
-var up = document.getElementById("up");
-up.addEventListener("click", function f() {
-    if (passwordLength < 20)
-        passwordLength++;
-    val.innerHTML = passwordLength;
-    getPassword();
-    result.value = word;
-    copy();
-    check();
-});
-
-var down = document.getElementById("down");
-down.addEventListener("click", function f() {
-    if (passwordLength > 0)
-        passwordLength--;
-    val.innerHTML = passwordLength;
-    getPassword();
-    result.value = word;
-    copy();
-    check();
-});
-
-//Clear selection
-function clearSelection() {
-    var sel;
-    if ((sel = document.selection) && sel.empty) {
-        sel.empty();
-    } else {
-        if (window.getSelection) {
-            window.getSelection().removeAllRanges();
-        }
-        var activeEl = document.activeElement;
-        if (activeEl) {
-            var tagName = activeEl.nodeName.toLowerCase();
-            if (tagName == "textarea" ||
-                (tagName == "input" && activeEl.type == "text")) {
-                // Collapse the selection to the end
-                activeEl.selectionStart = activeEl.selectionEnd;
-            }
-        }
-    }
-}
-
-//Check password
-function check() {
-    if (includeLowerCase && includeUpperCase && includeNumbers && includeSymbols && passwordLength > 7) {
-        strength.innerHTML = "STRONG ✓";
-        strength.style.color = "#00b894";
-    }
-    else {
-        strength.innerHTML = "WEAK ✗";
-        strength.style.color = "#d63031";
-    }
-}
-
-//On load
-getPassword();
-result.value = word;
-copy();
-check();
+setTimeout(function () { splashScreen.style.display = "none"; }, 1000);
